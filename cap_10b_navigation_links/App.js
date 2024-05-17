70d17714d9abca795ad275840d80378be6383072
Mitsuaki Tanaka <130664643+mitsuTanak@users.noreply.github.com>
2024-05-17 12:32:28 -0300

    a

--- cap_10b_navigation_links/App.js
+++ cap_10b_navigation_links/App.js
@@ -3,16 +3,18 @@ import Evento from './components/Evento';
 import UsuarioGithub from './components/UsuarioGithub';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
+import { View } from 'react-native-reanimated/lib/typescript/Animated';
+import { Button } from 'react-native';
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name="Evento" component={Evento} />
-        <Drawer.Screen name="Github" component={UsuarioGithub} />
-      </Drawer.Navigator>
-    </NavigationContainer>
+    <View style={Estilos.container}>
+      <Button 
+        title='Evento'
+        onPress={() =>}
+      />
+    </View>
   );
 }
